cf20b43f55e70234ec686183dc4bba62f24eee01
B1Fr0st <lovewolves413@gmail.com>
2022-04-29 15:21:36 -0700

    Add timer for dominant color detection

--- collage.py
+++ collage.py
@@ -2,7 +2,7 @@
 import dominantcolor as domCol
 import filetrawler as fileTrawl
 from PIL import Image
-
+from time import time
 masterImage = "master.png"
 
 images = "images\\"
@@ -10,8 +10,12 @@ images = fileTrawl.getFiles(images)
 domImages = {}
 
 for image in images:
+    start = time()
     domImages[image] = [domCol.returnDominantColor(image),False]
-    print(f"Dominant color found for image {image}:{domImages[image]}")
+    finish = time()
+    print(f"Dominant color found for image {image}:{domImages[image]}.")
+    print(f"Time taken:{finish-start}.")
+    
     #The bool was so that we can tell whether or not this image has been used before.
     #This is so that we only reuse images if we have to.
 
